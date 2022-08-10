4b1e275a8f3f14304fe2a0dc58564f1b25586021
Katy Long <katy.long@wearecodenation.com>
2022-08-10 17:15:37 +0100

    simple list addition

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [task, setTask] = useState('');
+  const [list, addToList] = useState([])
+
+  const handleChange = event => {
+    setTask(event.target.value);
+  };
+
+  const taskAdd = (task) =>{
+    let allTasks = [...list]
+    allTasks.push(task)
+    addToList(allTasks)
+    console.log(allTasks)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" id="newTask" name="newTask" onChange={handleChange} value={task}/>
+
+      <h2>Task: {task}</h2>
+      <button onClick={()=> taskAdd(task)}>Save item</button>
     </div>
   );
-}
+};
 
 export default App;
+
+// default State, empty list?
+// box for accepting text for to do list?
+// each add pushes a new item to the state list? (doing the copy cos you can't mutate it directly)
+// then each item can be refered to by the index to be ticked off or deleted?
+
+// conditional rendering? if done button add strikethrough, if delete button display: none?
+
+// how do you make it so it doesn't add everything? How can you collect the value on click? 
+
+// sounds easy when you block it out like that
